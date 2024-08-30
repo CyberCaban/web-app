79d98a2afc14e21996313728ba58654482ac0406
CyberCaban <andrewgamer63@gmail.com>
2024-08-30 09:40:57 +0300

    env variables

--- src/config.rs
+++ src/config.rs
@@ -5,7 +5,7 @@ use std::env;
 pub fn from_env() -> Config {
     dotenv().ok();
     let port = env::var("PORT").unwrap_or_else(|_| "8000".to_string());
-    let address = env::var("ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
+    let address = env::var("ROCKET_ADDRESS").unwrap_or_else(|_| "127.0.0.1".to_string());
 
     let config = Config {
         address: address.parse().unwrap(),
